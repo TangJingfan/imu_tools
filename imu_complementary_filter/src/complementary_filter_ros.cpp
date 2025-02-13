b0ca766d9563d090f111f30fcc396c7505dcde4f
tang_jingfan <tangjingfan@gmail.com>
2025-02-13 23:08:46 +0800

    fix: topic name

--- imu_complementary_filter/src/complementary_filter_ros.cpp
+++ imu_complementary_filter/src/complementary_filter_ros.cpp
@@ -66,8 +66,7 @@ ComplementaryFilterROS::ComplementaryFilterROS(
     }
 
     // Register IMU raw data subscriber.
-    imu_subscriber_.reset(new ImuSubscriber(
-        nh_, ros::names::resolve("imu") + "/data_raw", queue_size));
+    imu_subscriber_.reset(new ImuSubscriber(nh_, "/imu_data_raw", queue_size));
 
     // Register magnetic data subscriber.
     if (use_mag_)
